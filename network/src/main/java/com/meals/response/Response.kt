c0e465a693cfd9873a5c0f90f912e5dc99d690a5
Yusuf Maulana <yusufmaulana@engineer.com>
2021-02-17 14:48:21 +0700

    - Refactor BaseResponse.kt

--- network/src/main/java/com/meals/response/Response.kt
+++ network/src/main/java/com/meals/response/Response.kt
@@ -1,11 +1,13 @@
 package com.meals.response
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.io.Serializable
 
-data class BaseResponse<T>(
+@JsonClass(generateAdapter = true)
+data class Response<T>(
     val page: Int = 0,
     @Json(name = "total_results") val totalResult: Int = 0,
     @Json(name = "total_pages") val totalPages: Int = 0,
-    @Json(name = "results") val result: T
+    @Json(name = "meals") val result: T
 ) : Serializable
