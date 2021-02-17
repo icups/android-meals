b42c701f3d9efb857f0e293be17c168a928500e7
Yusuf Maulana <yusufmaulana@engineer.com>
2021-02-17 15:03:45 +0700

    - Refactor services - Add meal detail to service

--- network/src/main/java/com/meals/services/MealServices.kt
+++ network/src/main/java/com/meals/services/MealServices.kt
@@ -6,11 +6,17 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface UserServices {
+interface MealServices {
 
-    @GET("{api_key}/random.php")
+    @GET("{api_key}/randomselection.php")
     suspend fun randomMeals(
         @Path("api_key") requestId: String
+    ): PaginatedResponse<Meal>
+
+    @GET("{api_key}/lookup.php")
+    suspend fun mealDetail(
+        @Path("api_key") requestId: String,
+        @Query("i") idMeal: String,
     ): PaginatedResponse<Meal>
 
 }
